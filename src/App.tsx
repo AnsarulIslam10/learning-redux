64dc63144c2c5bdb7a2398ce67962c245a2c2fe1
AnsarulIslam10 <ansarulislam01739@gmail.com>
2025-06-28 10:12:31 +0600

    initializing shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,10 @@
-import { decrement, increment } from "./redux/features/counter/counterSlice";
-import { useAppDispatch, useAppSelector } from "./redux/hook";
+import { Button } from "./components/ui/button";
 
 function App() {
-  const dispatch = useAppDispatch();
-  const {count} = useAppSelector((state)=> state.counter)
-
-  const handleIncrement = (amount: number) => {
-    dispatch(increment(amount));
-  };
-  const handleDecrement = () => {
-    dispatch(decrement());
-  };
-  return (
-    <div>
-      <h1>Counter With Redux</h1>
-      <button onClick={()=>handleIncrement(5)}>Increment by 5</button>
-      <button onClick={()=>handleIncrement(1)}>Increment</button>
-      <div>{count}</div>
-      <button onClick={handleDecrement}>Decrement</button>
-    </div>
-  );
+  return <div>
+    <h1 className="text-5xl">Besic todo app</h1>
+    <Button>Test</Button>
+  </div>;
 }
 
 export default App;
