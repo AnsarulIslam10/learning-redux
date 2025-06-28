d6308e672d7936e0006b0e388bfcd8c7efb3989e
AnsarulIslam10 <ansarulislam01739@gmail.com>
2025-06-28 22:19:53 +0600

    dark mode applied

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,13 +1,17 @@
 import Logo from "@/assets/clover.png";
 import { Link } from "react-router";
+import { ModeToggle } from "../ui/mode-toggler";
 export default function Navbar() {
   return (
-    <nav className="max-w-7xl mx-auto h-16 flex items-center gap-3 px-5">
+    <nav className="max-w-7xl mx-auto h-16 flex items-center justify-between gap-3 px-5">
       <div className="flex items-center">
         <img src={Logo} alt="" className="w-16" /><span className="font-bold -ml-3">Task</span>Master
       </div>
+      <div>
       <Link to={"/"} >Tasks</Link>
       <Link to={"/users"} >Users</Link>
+      </div>
+      <div><ModeToggle></ModeToggle></div>
     </nav>
   );
 }
