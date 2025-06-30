2f664a64a2f4aae530b992a97a9a5f70ca51ebf6
AnsarulIslam10 <ansarulislam01739@gmail.com>
2025-06-30 09:39:23 +0600

    Managing Task Completion and Deletion

--- src/redux/features/task/taskSlice.ts
+++ src/redux/features/task/taskSlice.ts
@@ -37,8 +37,8 @@ const initialState: IinitialState = {
 
 type DraftTask = Pick<ITask, 'title' | "description" | "dueDate" | "priority">;
 
-const createTask = (taskData: DraftTask) : ITask =>{
-return { id: nanoid(), isCompleted: false, ...taskData }
+const createTask = (taskData: DraftTask): ITask => {
+    return { id: nanoid(), isCompleted: false, ...taskData }
 }
 
 
@@ -46,9 +46,16 @@ const taskSlice = createSlice({
     name: "task",
     initialState,
     reducers: {
-        addTask : (state, action: PayloadAction<DraftTask>)=> {
+        addTask: (state, action: PayloadAction<DraftTask>) => {
             const taskData = createTask(action.payload)
             state.tasks.push(taskData);
+        },
+        toggleCompleteState: (state, action: PayloadAction<string>) => {
+            console.log(action)
+            state.tasks.forEach((task) => task.id === action.payload ? task.isCompleted = !task.isCompleted : task)
+        },
+        deleteTask: (state, action: PayloadAction<string>) => {
+            state.tasks = state.tasks.filter(task => task.id !== action.payload)
         }
     }
 })
@@ -60,6 +67,6 @@ export const selectFilter = (state: RootState) => {
     return state.todo.filter
 }
 
-export const {addTask} = taskSlice.actions;
+export const { addTask, toggleCompleteState, deleteTask } = taskSlice.actions;
 
 export default taskSlice.reducer;
